51794f9126d4ea32ff9808f1d6d556b933abbbcc
golnazhabibi3 <golnaz.habibi@gmail.com>
2020-07-15 06:55:37 -0400

    added golnaz stuff

--- partition/Partitioner.py
+++ partition/Partitioner.py
@@ -1,6 +1,9 @@
 import numpy as np
 from itertools import product
 from partition.xiang import sect, bisect
+from partition.object_boundary import getboundary
+from sklearn.neighbors import KDTree
+from sklearn.neighbors import NearestNeighbors
 
 
 class Partitioner():
@@ -169,4 +172,413 @@ class GreedySimGuidedPartitioner(SimGuidedPartitioner):
 
         return input_range_, output_range_
 
+class BoundarySimGuidedPartitioner(Partitioner):
+    def __init__(self, N=20, tolerance_eps=0.01):
+        Partitioner.__init__(self)
+        self.N= N
+        self.tolerance_eps= tolerance_eps
 
+    def get_output_range(self, input_range, propagator):
+        input_shape = input_range.shape[:-1]
+        num_propagator_calls = 0
+
+        interior_M = []
+        M=[]
+        M_inside=[]
+        info = {}
+        boundary_shape="convex"
+        sect_method = 'max'
+        num_inputs = input_range.shape[0]
+        num_partitions = (self.N)*np.ones((num_inputs,), dtype=int)
+        slope = np.divide((input_range[:,1] - input_range[:,0]), num_partitions)
+        sampling_cell = np.empty(((self.N)**2,num_inputs,2))
+        output_range_ = np.empty(((self.N)**2,num_inputs,2))
+        sampled_output_ = np.empty(((self.N)**2,num_inputs))
+        sampled_output_boundary_ = np.empty(((self.N)**2))
+
+   
+        input_range_ = np.empty((self.N**2,num_inputs,2))
+
+        element=-1
+        for idx in product(*[range(num) for num in num_partitions]):
+            element = element+1
+            input_range_[element,:,0] = input_range[:,0]+np.multiply(idx, slope)
+            input_range_[element,:,1] = input_range[:,0]+np.multiply(np.array(idx)+1, slope)
+            sampled_input_= np.random.uniform(input_range_[element,:,0], input_range_[element,:,1], (1,num_inputs,))
+            sampled_output_[element,:] =  propagator.forward_pass( propagator.forward_pass(sampled_input_))
+            sampled_output_boundary_[element] =0;
+           # output_range_[element,:,:],_= propagator.get_output_range(input_range_[element,:,:])
+           # num_propagator_calls += 1
+
+        if boundary_shape=="convex":
+            boundary_points= getboundary(sampled_output_,0.0)
+        else:
+            boundary_points= getboundary(sampled_output_,0.4)
+
+        for i in range(self.N**2):
+            if sampled_output_[i,:] in boundary_points:
+                sampled_output_boundary_[i]=1
+                propagator.get_output_range(input_range_[i,:,:])
+                num_propagator_calls += 1
+                M.append((input_range_[i,:,:], output_range_[i,:,:], sampled_output_boundary_[i]))# (Line 4)  
+
+            else:
+                M_inside.append((input_range_[i,:,:]))    
+
+        
+
+        output_range_sim = np.empty_like(output_range_[0,:,:])
+        output_range_sim[:,0] = np.min(sampled_output_, axis=0)
+        output_range_sim[:,1] = np.max(sampled_output_, axis=0)
+            
+        u_e = np.empty_like(output_range_sim)
+        u_e[:,0] = np.inf
+        u_e[:,1] = -np.inf
+
+
+        M_e = np.empty_like(output_range_sim)
+        M_e[:,0] = np.inf
+        M_e[:,1] = -np.inf
+
+        while len(M) != 0:
+            input_range_, output_range_,sampled_output_boundary_ = M.pop(0) # Line 9
+            if np.all((output_range_sim[:,0] - output_range_[:,0]) <= 0) and \
+            np.all((output_range_sim[:,1] - output_range_[:,1]) >= 0) or sampled_output_boundary_==0:
+            # Line 11
+                tmp = np.dstack([u_e, output_range_])
+                u_e[:,1] = np.max(tmp[:,1,:], axis=1)
+                u_e[:,0] = np.min(tmp[:,0,:], axis=1)
+                interior_M.append((input_range_, output_range_))
+            else:
+            # Line 14
+                if np.max(input_range_[:,1] - input_range_[:,0]) > self.tolerance_eps:
+                # Line 15
+                    input_ranges_ = sect(input_range_, 2, select=sect_method)
+                # Lines 16-17
+                    for input_range_ in input_ranges_:
+                        output_range_,_= propagator.get_output_range(input_range_)
+                        num_propagator_calls += 1
+   
+                        M.append((input_range_, output_range_,sampled_output_boundary_)) # Line 18
+                else: # Lines 19-20
+                    interior_M.append((input_range_, output_range_))
+                    break
+
+
+        if len(M) > 0:
+            M_numpy = np.dstack([output_range_ for (_, output_range_,_) in M])
+            M_range = np.empty_like(u_e)
+            M_range[:,1] = np.max(M_numpy[:,1,:], axis=1)
+            M_range[:,0] = np.min(M_numpy[:,0,:], axis=1)
+    
+            tmp1 = np.dstack([u_e, M_range])
+            u_e[:,1] = np.max(tmp1[:,1,:], axis=1)
+            u_e[:,0] = np.min(tmp1[:,0,:], axis=1)
+        M1=[]
+        if len(M_inside) > 0:
+            for (input_range) in M_inside:
+                output_range_,_= propagator.get_output_range(input_range_)
+                num_propagator_calls += 1
+                M2 = np.dstack([M_e, output_range_])
+                M_e[:,1] = np.max(M2[:,1,:], axis=1)
+                M_e[:,0] = np.min(M2[:,0,:], axis=1)
+
+            tmp2 = np.dstack([u_e, M_e])
+            u_e[:,1] = np.max(tmp2[:,1,:], axis=1)
+            u_e[:,0] = np.min(tmp2[:,0,:], axis=1)
+    
+
+     
+        info["all_partitions"] = M+interior_M
+        info["exterior_partitions"] = M
+        info["interior_partitions"] = interior_M
+        info["num_propagator_calls"] = num_propagator_calls
+        info["num_partitions"] = len(M) + len(interior_M)
+        return u_e, info
+
+class AdaptiveSimGuidedPartitioner(Partitioner):
+    def __init__(self, N=20, k_NN =1, tolerance_eps=0.01, tolerance_expanding_step=0.001,tolerance_range=0.05):
+        Partitioner.__init__(self)
+        self.N= N
+        self.tolerance_eps= tolerance_eps
+        self.tolerance_step= tolerance_expanding_step
+        self.k_NN= k_NN
+        self.tolerance_range= tolerance_range
+
+   # def grab_from_M(self, M, output_range_sim):
+       # input_range_, output_range_ = M.pop(0) 
+        #return input_range_, output_range_
+    def set_delta_step(self,range_,center_value, idx, stage=1):
+        c=0.5
+        if stage==1:
+            k=-1
+            num_inputs=idx
+            delta_step =np.ones((num_inputs*2,1))
+            output_range = range_
+            distance = np.empty((len(range_)*2))
+            output_center = center_value
+            output_bounding_box = [[output_range[0,0],output_range[1,0]],[output_range[0,1],output_range[1,0]],
+            [output_range[0,1],output_range[1,1]],[output_range[0,0],output_range[1,1]]]
+            for (i,j) in output_bounding_box:
+                k=k+1
+                distance[k] = np.sqrt((center_value[0]-i)**2 + (center_value[1]-j)**2 )
+            min_distance = np.min(distance)
+            for i in range(len(range_)*2):
+                if i % 2 == 0: 
+                    delta_step[i]= -c*min_distance/np.max(output_range[:,1]-output_range[:,0])
+                else:
+                    delta_step[i]= c*min_distance/np.max(output_range[:,1]-output_range[:,0])
+        else:
+            input_range = range_
+            diff_rolled = center_value
+            num_inputs = len(range_)
+            delta_step =np.zeros((num_inputs*2,1))
+            if idx % 2 == 0: 
+                delta_step[idx]= -c*diff_rolled[idx]/(np.max(input_range[:,0]))
+            else:
+                delta_step[idx]= c*diff_rolled[idx]/(np.max(input_range[:,1]))
+        return delta_step
+
+    def get_output_range(self, input_range, propagator):
+ 
+    #tolerance_range=0.05
+        num_propagator_calls=0
+        interior_M = []
+        info = {}
+        boundary_shape="convex"
+        sect_method = 'max'
+        num_inputs = input_range.shape[0]
+        num_partitions = (self.N)*np.ones((num_inputs,), dtype=int)
+        slope = np.divide((input_range[:,1] - input_range[:,0]), num_partitions)
+        sampling_cell = np.empty(((self.N)**2,num_inputs,2))
+        output_range_ = np.empty(((self.N)**2,num_inputs,2))
+        sampled_output_ = np.empty(((self.N)**2,num_inputs))
+
+        sampled_output_boundary_ = np.empty(((self.N)**2))
+
+
+   
+        M=[]
+        element=-1
+
+        for idx in product(*[range(num) for num in num_partitions]):
+           element = element+1
+           sampling_cell[element,:,0] = input_range[:,0]+np.multiply(idx, slope)
+           sampling_cell[element,:,1] = input_range[:,0]+np.multiply(np.array(idx)+1, slope)
+           sampled_input_= np.random.uniform(sampling_cell[element,:,0], sampling_cell[element,:,1], (1,num_inputs,))
+           sampled_output_[element,:] =  propagator.forward_pass( propagator.forward_pass(sampled_input_))
+           sampled_output_boundary_[element] =0;
+    
+        output_range_sim = np.empty(sampled_output_.shape[1:]+(2,))
+        output_range_sim[:,0] = np.min(sampled_output_, axis=0)
+        output_range_sim[:,1] = np.max(sampled_output_, axis=0)
+
+
+        if boundary_shape=="convex":
+            boundary_points= getboundary(sampled_output_,0.0)
+        else:
+            boundary_points= getboundary(sampled_output_,0.4)
+  
+        sampled_output_center=boundary_points.mean(axis=0)
+
+    
+        input_range_initial = np.empty((num_inputs, 2))
+
+        kdt = KDTree(sampled_output_, leaf_size=30, metric='euclidean')
+        center_NN=kdt.query(sampled_output_center.reshape(1,-1), k=self.k_NN, return_distance=False)
+
+        NN_range_x=[]
+        NN_range_y=[]
+
+        for i in center_NN:
+            NN_range_x.append(sampling_cell[i,0,:])
+            NN_range_y.append(sampling_cell[i,1,:])
+
+        input_range_initial[0,0] = np.min(NN_range_x)
+        input_range_initial[1,0] = np.min(NN_range_y)
+        input_range_initial[0,1] = np.max(NN_range_x)
+        input_range_initial[1,1] = np.max(NN_range_y)
+    
+        terminating_condition=False
+    
+        input_range_new = np.empty((num_inputs, 2))
+        input_range_new[0,0] = input_range_initial[0,0]
+        input_range_new[0,1] = input_range_initial[0,1]
+        input_range_new[1,0]  = input_range_initial[1,0]
+        input_range_new[1,1] = input_range_initial[1,1]
+        delta_step = self.set_delta_step(output_range_sim,sampled_output_center, num_inputs, stage=1)
+        prev_range =np.inf
+
+        count =0
+        M=[]
+        while terminating_condition==False:
+ 
+            input_range_new= input_range_new+delta_step.reshape((num_inputs, 2))
+            output_range_new,_= propagator.get_output_range(input_range_new)
+
+            num_propagator_calls += 1
+            if np.all((output_range_sim[:,0] - output_range_new[:,0]) <= 0) and \
+            np.all((output_range_sim[:,1] - output_range_new[:,1]) >= 0):
+                terminating_condition=False
+
+            else:
+                input_range_new= input_range_new-delta_step.reshape((num_inputs, 2))
+                break
+                #delta_step=delta_step/2
+
+                tol = 5
+                if np.max(abs(delta_step))<self.tolerance_step:
+
+                    diff=(input_range-input_range_new)
+
+                    max_range= (np.max(abs(diff)))
+                    if max_range<self.tolerance_range:# or abs(max_range-prev_range)<tol:
+                                 
+                        if input_range[0,0] !=input_range_new[0,0]: 
+
+                            input_range_ = np.array([[input_range[0,0] ,input_range_new[0,0]],[input_range[1,0], input_range[1,1]]])
+                            output_range_,_= propagator.get_output_range(input_range_)
+                            num_propagator_calls += 1
+   
+                            M.append((input_range_,output_range_)) 
+
+                        if[input_range_new[0,1]!=input_range[0,1]]:
+
+                            input_range_ = np.array([[input_range_new[0,1],input_range[0,1]],[input_range[1,0],input_range[1,1]]])
+                            output_range_,_= propagator.get_output_range(input_range_)
+                            num_propagator_calls += 1
+                            M.append((input_range_,output_range_)) 
+
+                        
+                        if[input_range_new[1,1]!=input_range[1,1]]:
+
+                            input_range_ = np.array([[input_range_new[0,0],input_range_new[0,1]],[input_range_new[1,1],input_range[1,1]]])
+                            output_range_,_= propagator.get_output_range(input_range_)
+                            num_propagator_calls += 1
+
+                            M.append((input_range_,output_range_))
+                           
+
+                        if[input_range_new[1,0]!=input_range[1,0]]:
+
+                            input_range_ = np.array([[input_range_new[0,0],input_range_new[0,1]],[input_range[1,0],input_range_new[1,0]]])
+                            output_range_,_= propagator.get_output_range(input_range_)
+                        
+                            num_propagator_calls += 1
+    
+                            M.append((input_range_,output_range_)) 
+       
+                        break
+                    else:
+                        diff_rolled= np.concatenate(abs(diff))
+                        if abs(max_range-prev_range)<tol:
+                            count  = count+1
+                        idx= num_inputs*2-1-count;
+                        prev_range= np.inf;
+                        if idx<0:
+
+                            break
+                        else:
+                            prev_range = max_range
+                            delta_step =self.set_delta_step(input_range, diff_rolled,idx, stage=2)  
+                            while np.max(abs(delta_step))<self.tolerance_step:
+                                idx-=1
+                                if idx<0:
+                                    break
+                                else:
+                                    delta_step =self.set_delta_step(input_range, diff_rolled,idx, stage=2)  
+
+
+        if input_range[0,0] !=input_range_new[0,0]: 
+
+            input_range_ = np.array([[input_range[0,0] ,input_range_new[0,0]],[input_range[1,0], input_range[1,1]]])
+            output_range_,_= propagator.get_output_range(input_range_)
+            num_propagator_calls += 1
+    
+            M.append((input_range_,output_range_)) 
+     
+
+        if[input_range_new[0,1]!=input_range[0,1]]:
+
+            input_range_ = np.array([[input_range_new[0,1],input_range[0,1]],[input_range[1,0],input_range[1,1]]])
+            output_range_,_= propagator.get_output_range(input_range_)
+            num_propagator_calls += 1
+   
+            M.append((input_range_,output_range_))
+                          
+                        
+        if[input_range_new[1,1]!=input_range[1,1]]:
+
+            input_range_ = np.array([[input_range_new[0,0],input_range_new[0,1]],[input_range_new[1,1],input_range[1,1]]])
+            output_range_,_= propagator.get_output_range(input_range_)
+            num_propagator_calls += 1
+
+            M.append((input_range_,output_range_))
+                           
+
+        if[input_range_new[1,0]!=input_range[1,0]]:
+
+            input_range_ = np.array([[input_range_new[0,0],input_range_new[0,1]],[input_range[1,0],input_range_new[1,0]]])
+            output_range_,_= propagator.get_output_range(input_range_)
+            num_propagator_calls += 1
+
+            M.append((input_range_,output_range_))        
+
+        
+
+        u_e = np.empty_like(output_range_sim)
+        u_e[:,0] = np.inf
+        u_e[:,1] = -np.inf
+
+        while len(M) != 0:
+            input_range_, output_range_ = M.pop(0) # Line 9
+            if np.all((output_range_sim[:,0] - output_range_[:,0]) <= 0) and \
+            np.all((output_range_sim[:,1] - output_range_[:,1]) >= 0):
+            # Line 11
+                tmp = np.dstack([u_e, output_range_])
+                u_e[:,1] = np.max(tmp[:,1,:], axis=1)
+                u_e[:,0] = np.min(tmp[:,0,:], axis=1)
+                interior_M.append((input_range_, output_range_))
+            else:
+            # Line 14
+                if np.max(input_range_[:,1] - input_range_[:,0]) > self.tolerance_eps:
+                # Line 15
+                    input_ranges_ = sect(input_range_, 2, select=sect_method)
+                # Lines 16-17
+                    for input_range_ in input_ranges_:
+                        output_range_,_= propagator.get_output_range(input_range_)
+                        num_propagator_calls += 1
+   
+                        M.append((input_range_, output_range_)) # Line 18
+                else: # Lines 19-20
+                    interior_M.append((input_range_, output_range_))
+                    break
+
+        M.append((input_range_new,output_range_new))
+
+        if len(M) > 0:
+            M_numpy = np.dstack([output_range_ for (_, output_range_) in M])
+            M_range = np.empty_like(u_e)
+            M_range[:,1] = np.max(M_numpy[:,1,:], axis=1)
+            M_range[:,0] = np.min(M_numpy[:,0,:], axis=1)
+    
+            tmp = np.dstack([u_e, M_range])
+            u_e[:,1] = np.max(tmp[:,1,:], axis=1)
+            u_e[:,0] = np.min(tmp[:,0,:], axis=1)
+    
+
+        #print('bounding box area ratio_gh:')
+  
+        #u_range = u_e[:,1]- u_e[:,0]
+
+        #sim_out_range= sampled_outputs_range[:,1]-sampled_outputs_range[:,0]
+        #print(u_range[0]*u_range[1]/(sim_out_range[0]*sim_out_range[1]))
+
+
+        info["all_partitions"] = M+interior_M
+        info["exterior_partitions"] = M
+        info["interior_partitions"] = interior_M
+        info["num_propagator_calls"] = num_propagator_calls
+        info["num_partitions"] = len(M) + len(interior_M)
+        
+        return u_e, info
--- partition/experiments.py
+++ partition/experiments.py
@@ -3,7 +3,7 @@ import partition
 import partition.Partitioner
 import partition.Analyzer
 import partition.Propagator
-from partition.xiang import model_xiang_2020_robot_arm
+from partition.xiang import model_xiang_2020_robot_arm, model_gh1
 import numpy as np
 import pandas as pd
 import itertools
@@ -19,11 +19,14 @@ partitioner_dict = {
     "Uniform": partition.Partitioner.UniformPartitioner,
     "SimGuided": partition.Partitioner.SimGuidedPartitioner,
     "GreedySimGuided": partition.Partitioner.GreedySimGuidedPartitioner,
+    "AdaptiveSimGuided": partition.Partitioner.AdaptiveSimGuidedPartitioner,
+    "BoundarySimGuided": partition.Partitioner.BoundarySimGuidedPartitioner,
+
 }
 propagator_dict = {
     "IBP": partition.Propagator.IBPPropagator,
     "CROWN": partition.Propagator.CROWNPropagator,
-    "SDP": partition.Propagator.SDPPropagator,
+    #"SDP": partition.Propagator.SDPPropagator,
 }
 
 save_dir = "{}/results".format(os.path.dirname(os.path.abspath(__file__)))
@@ -32,15 +35,17 @@ os.makedirs(save_dir, exist_ok=True)
 def experiment():
     
     # Choose the model and input range
+    torch_model = model_gh1()
     torch_model = model_xiang_2020_robot_arm()
+
     input_range = np.array([ # (num_inputs, 2)
                       [np.pi/3, 2*np.pi/3], # x0min, x0max
                       [np.pi/3, 2*np.pi/3] # x1min, x1max
     ])
     
     # Select which algorithms and hyperparameters to evaluate
-    partitioners = ["Uniform", "SimGuided", "GreedySimGuided"]
-    propagators = ["IBP", "CROWN", "SDP"]
+    partitioners = ["Uniform", "SimGuided", "GreedySimGuided", "AdaptiveSimGuided"]
+    propagators = ["IBP", "CROWN"]
     partitioner_hyperparams_to_use = {
         "Uniform":
             {
@@ -55,6 +60,16 @@ def experiment():
             {
                 "tolerance_eps": [1.0, 0.5, 0.2, 0.1, 0.05, 0.01],
                 "num_simulations": [1000]
+            },
+        "AdaptiveSimGuided":
+        #"BoundarySimGuided":
+
+            {
+                "tolerance_eps": [1.0, 0.5, 0.2, 0.1, 0.05,0.03],
+                "tolerance_expanding_step": [0.0001],
+                "k_NN": [1],
+                "N": [20],
+                "tolerance_range": [0.65]
             },
     }
 
@@ -158,6 +173,11 @@ algs ={
         "color_ind": 2,
         "name": "GreedySimGuided",
     },
+    "AdaptiveSimGuidedPartitioner": {
+        "marker": "*",
+        "color_ind": 3,
+        "name": "BoundarySimGuided",
+    },
     "IBPPropagator": {
         "color_ind": 0,
         "name": "IBPPropagator",
@@ -177,7 +197,7 @@ algs ={
 if __name__ == '__main__':
 
     # Run an experiment
-    # df = experiment()
+    df = experiment()
 
     # If you want to plot w/o re-running the experiments, comment out the experiment line.
     if 'df' not in locals():
