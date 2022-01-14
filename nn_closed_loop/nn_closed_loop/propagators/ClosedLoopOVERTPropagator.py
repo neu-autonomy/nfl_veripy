a2935dcda4eefa932405c913c864bddaa914c302
Michael Everett <mfe@mit.edu>
2022-01-13 20:23:50 -0500

    closer to real dynamics

--- nn_closed_loop/nn_closed_loop/propagators/ClosedLoopOVERTPropagator.py
+++ nn_closed_loop/nn_closed_loop/propagators/ClosedLoopOVERTPropagator.py
@@ -7,6 +7,12 @@ from nn_closed_loop.utils.utils import range_to_polytope
 import requests
 import json
 from copy import deepcopy
+import os
+
+model_dir = "{}/../../models/nnet/".format(
+    os.path.dirname(os.path.abspath(__file__))
+)
+os.makedirs(model_dir, exist_ok=True)
 
 
 class ClosedLoopOVERTPropagator(ClosedLoopPropagator):
@@ -15,26 +21,59 @@ class ClosedLoopOVERTPropagator(ClosedLoopPropagator):
             self, input_shape=input_shape, dynamics=dynamics
         )
         self.url = 'http://localhost:8000/overt'
+        self.model_filename = model_dir+"/tmp_model.nnet"
 
     def torch2network(self, torch_model):
 
-        import pdb; pdb.set_trace()
+        # Write a .nnet file based on the torch_model
+        act = None
+        dims = []
+        with open(self.model_filename, "a") as f:
+            f.truncate(0)
 
-        with open("test.txt", "ab") as f:
-            for layer in torch_model:
-                w = torch_model[layer].weight.data.numpy()
-                b = torch_model[layer].bias.data.numpy()
-                numpy.savetxt(f, a)
+            for idx, m in enumerate(torch_model.modules()):
+                if isinstance(m, torch.nn.Sequential):
+                    continue
+                elif isinstance(m, torch.nn.ReLU):
+                    if act is None or act == "relu":
+                        act = "relu"
+                    else:
+                        print(
+                            "Multiple types of activations in your model --- unsuported by robust_sdp."
+                        )
+                        assert 0
+                elif isinstance(m, torch.nn.Linear):
+                    dims.append(m.in_features)
+                else:
+                    print("That layer isn't supported.")
+                    assert 0
+            dims.append(m.out_features)
+            f.write(str(len(dims) - 1)+'\n')
+            f.write(', '.join([str(d) for d in dims])+'\n')
+            for i in range(5): f.write('0\n')
 
+            for idx, m in enumerate(torch_model.modules()):
+                if isinstance(m, torch.nn.Sequential):
+                    continue
+                elif isinstance(m, torch.nn.ReLU):
+                    if act is None or act == "relu":
+                        act = "relu"
+                    else:
+                        print(
+                            "Multiple types of activations in your model --- unsuported by robust_sdp."
+                        )
+                        assert 0
+                elif isinstance(m, torch.nn.Linear):
+                    w = m.weight.data.numpy()
+                    np.savetxt(f, w, fmt="%s")
+                    b = m.bias.data.numpy()
+                    np.savetxt(f, b, fmt="%s")
+                else:
+                    print("That layer isn't supported.")
+                    assert 0
 
-
+        # Internally, we'll just use the typical torch stuff
         return torch_model
-        # from nn_closed_loop.utils.nn_bounds import BoundClosedLoopController
-
-        # torch_model_cl = BoundClosedLoopController.convert(
-        #     torch_model, dynamics=self.dynamics, bound_opts=self.params
-        # )
-        # return torch_model_cl
 
     def forward_pass(self, input_data):
         return self.network(
@@ -54,6 +93,7 @@ class ClosedLoopOVERTPropagator(ClosedLoopPropagator):
                     'high': input_constraint.range[..., 1].tolist(),
                 },
                 'num_timesteps': num_timesteps,
+                'controller': self.model_filename,
             }
             response = requests.post(self.url, data = json.dumps(data))
             ranges_list = json.loads(response.text)["result"]
--- run_overt.jl
+++ run_overt.jl
@@ -1,51 +1,12 @@
+
+##################################
+# Simple query of OVERT for closed-loop reachability
+##################################
+
 using OVERTVerify
 using LazySets
-using Joseki, JSON, HTTP
 
-### Create some endpoints
-
-# This function takes two numbers x and y from the query string and returns x^y
-# In this case they need to be identified by name and it should be called with
-# something like 'http://localhost:8000/pow/?x=2&y=3'
-function pow(req::HTTP.Request)
-    j = HTTP.queryparams(HTTP.URI(req.target))
-    has_all_required_keys(["x", "y"], j) || return error_responder(req, "You need to specify values for x and y!")
-    # Try to parse the values as numbers.  If there's an error here the generic
-    # error handler will deal with it.
-    x = parse(Float32, j["x"])
-    y = parse(Float32, j["y"])
-    json_responder(req, x^y)
-end
-
-# This function takes two numbers n and k from a JSON-encoded request
-# body and returns binomial(n, k)
-function bin(req::HTTP.Request)
-    j = try
-        body_as_dict(req)
-    catch err
-        return error_responder(req, "I was expecting a json request body!")
-    end
-    has_all_required_keys(["n", "k"], j) || return error_responder(req, "You need to specify values for n and k!")
-    json_responder(req, binomial(j["n"],j["k"]))
-end
-
-# This function takes two numbers n and k from a JSON-encoded request
-# body and returns binomial(n, k)
-function overtabc(req::HTTP.Request)
-    j = try
-        body_as_dict(req)
-    catch err
-        return error_responder(req, "I was expecting a json request body!")
-    end
-    has_all_required_keys(["input_set", "num_timesteps"], j) || return error_responder(req, "You need to specify other values!")
-
-    ranges = setup_overt(j["input_set"], j["num_timesteps"])
-
-    json_responder(req, ranges)
-end
-
-function setup_overt(input_set, num_timesteps)
-    controller = "/home/mfe/code/OVERTVerify.jl/nnet_files/jmlr/single_pendulum_small_controller.nnet"
+function setup_overt(input_set, num_timesteps, controller)
 
     query = OvertQuery(
         DoubleIntegrator,  # problem
@@ -71,71 +32,23 @@ function setup_overt(input_set, num_timesteps)
     return concrete_state_sets_range
 end
 
-
-###############
-
-using OVERT
-
-pend_mass, pend_len, grav_const, friction = 0.5, 0.5, 1., 0.
-
-function single_pend_dynamics(x::Array{T, 1} where {T <: Real},
-                              u::Array{T, 1} where {T <: Real})
-    dx1 = x[2]
-    dx2 = grav_const/pend_len * sin(x[1]) + 1 / (pend_mass*pend_len^2) * (u[1] - friction * x[2])
-    return [dx1, dx2]
-end
-
-single_pend_θ_doubledot = :($(grav_const/pend_len) * sin(x1) + $(1/(pend_mass*pend_len^2)) * u1 - $(friction/(pend_mass*pend_len^2)) * x2)
-
-function single_pend_dynamics_overt(range_dict::Dict{Symbol, Array{T, 1}} where {T <: Real},
-                                    N_OVERT::Int,
-                                    t_idx::Union{Int, Nothing}=nothing)
-    if isnothing(t_idx)
-        v1 = single_pend_θ_doubledot
-    else
-        v1 = "$(grav_const/pend_len) * sin(x1_$t_idx) + $(1/(pend_mass*pend_len^2)) * u1_$t_idx - $(friction/(pend_mass*pend_len^2)) * x2_$t_idx"
-        v1 = Meta.parse(v1)
-    end
-    v1_oA = overapprox(v1, range_dict; N=N_OVERT)
-    return v1_oA, [v1_oA.output]
-end
-
-function single_pend_update_rule(input_vars::Array{Symbol, 1},
-                                 control_vars::Array{Symbol, 1},
-                                 overt_output_vars::Array{Symbol, 1})
-    ddth = overt_output_vars[1]
-    integration_map = Dict(input_vars[1] => input_vars[2], input_vars[2] => ddth)
-    return integration_map
-end
-
-single_pend_input_vars = [:x1, :x2]
-single_pend_control_vars = [:u1]
-
-SinglePendulumABC = OvertProblem(
-    single_pend_dynamics,
-    single_pend_dynamics_overt,
-    single_pend_update_rule,
-    single_pend_input_vars,
-    single_pend_control_vars
-)
-
-
-##################
-
-###############
+##################################
+# double integrator dynamics
+##################################
 
 using OVERT
 using OVERT: add_overapproximate
+using Base.Math: clamp
 
 function double_integrator_dynamics(x::Array{T, 1} where {T <: Real},
                               u::Array{T, 1} where {T <: Real})
-    dx1 = x[1] + x[2] + 0.5*u[1]
-    dx2 = x[2] + u[1]
+    dx1 = x[2] + 0.5*clamp(u[1], -1, 1)
+    dx2 = clamp(u[1], -1, 1)
     return [dx1, dx2]
 end
 
-tmp_tbd = :(x1 + x2 + 0.5*u1)
-tmp_tbd2 = :(x2 + u1)
+tmp_tbd = :(x2 + 0.5*clamp(u1, -1, 1))
+tmp_tbd2 = :(clamp(u1, -1, 1))
 
 function double_integrator_dynamics_overt(range_dict::Dict{Symbol, Array{T, 1}} where {T <: Real},
                                     N_OVERT::Int,
@@ -144,9 +57,9 @@ function double_integrator_dynamics_overt(range_dict::Dict{Symbol, Array{T, 1}} 
         v1 = tmp_tbd
         v2 = tmp_tbd2
     else
-        v1 = "x1_$t_idx + x2_$t_idx + 0.5*u1_$t_idx"
+        v1 = "x2_$t_idx + 0.5*clamp(u1_$t_idx, -1, 1)"
         v1 = Meta.parse(v1)
-        v2 = "x2_$t_idx + u1_$t_idx"
+        v2 = "clamp(u1_$t_idx, -1, 1)"
         v2 = Meta.parse(v2)
     end
     v1_oA = overapprox(v1, range_dict; N=N_OVERT)
@@ -178,19 +91,38 @@ DoubleIntegrator = OvertProblem(
 )
 
 
-##################
+##################################
+# call w/o http server
+##################################
 
 # input_set = Dict("low" => [0.5, 0.6], "high" => [0.7, 0.8])
 # num_timesteps = 2
-# concrete_sets = setup_overt(input_set, num_timesteps)
+# controller = "/home/mfe/code/OVERTVerify.jl/nnet_files/jmlr/single_pendulum_small_controller.nnet"
+# concrete_sets = setup_overt(input_set, num_timesteps, controller)
 # print(concrete_sets)
 
-# ### Create and run the server
+##################################
+# enable calling julia over http server
+##################################
+
+### Create and run the server
+using Joseki, JSON, HTTP
+
+function overtabc(req::HTTP.Request)
+    j = try
+        body_as_dict(req)
+    catch err
+        return error_responder(req, "I was expecting a json request body!")
+    end
+    has_all_required_keys(["input_set", "num_timesteps"], j) || return error_responder(req, "You need to specify other values!")
+
+    ranges = setup_overt(j["input_set"], j["num_timesteps"])
+
+    json_responder(req, ranges)
+end
 
 # Make a router and add routes for our endpoints.
 endpoints = [
-    (pow, "GET", "/pow"),
-    (bin, "POST", "/bin"),
     (overtabc, "POST", "/overt")
 ]
 r = Joseki.router(endpoints)
