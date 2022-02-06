7cffbfd7b12536538bcdfff922e3d712b270a93f
Michael Everett <mfe@mit.edu>
2022-02-06 14:30:34 -0500

    fix bug in num_control_inputs

--- nn_closed_loop/nn_closed_loop/propagators/ClosedLoopCROWNIBPCodebasePropagator.py
+++ nn_closed_loop/nn_closed_loop/propagators/ClosedLoopCROWNIBPCodebasePropagator.py
@@ -201,7 +201,8 @@ class ClosedLoopCROWNIBPCodebasePropagator(ClosedLoopPropagator):
             u_max = self.dynamics.u_limits[:, 1]
 
         num_states = xt1_min.shape[0]
-        num_control_inputs = self.dynamics.bt.shape[0]
+        num_control_inputs = self.dynamics.bt.shape[1]
+        print(num_control_inputs)
         xt = cp.Variable(xt1_min.shape+(2,))
         ut = cp.Variable(num_control_inputs)
 
